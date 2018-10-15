20f861e2f7dfc4241c3237a53a194fd201bae1ff
Anastasija Pischik <nastijpischik@gmail.com>
2018-10-15 09:38:48 +0300

    v1

---
+++ ConsumingData/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace ConsumingData
+{
+    [Serializable]
+    public class Book
+    {
+        [XmlAttribute]
+        public string Title { set; get; }
+        [XmlAttribute]
+        public string Author { set; get; }
+        [XmlElement]
+        public int NumberOfPages { set; get; }
+        [XmlElement]
+        public int PublishingYear { set; get; }
+
+        public Book()
+        {
+        }
+
+        public Book(string title, string author, int numberOfPages, int publishingYear)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+            PublishingYear = publishingYear;
+        }
+
+        public void ReadFromXml(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Book));
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                Book book = (Book)serializer.Deserialize(streamReader);
+                Title = book.Title;
+                Author = book.Author;
+                NumberOfPages = book.NumberOfPages;
+                PublishingYear = book.PublishingYear;
+            }         
+        }
+
+        public void SaveToXml(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Book));
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                serializer.Serialize(streamWriter, this);
+            }
+        }
+
+        public void SaveToJSON(string path)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(path))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, this);
+                }
+        }
+
+        public void LoadFromJSON(string path)
+        {
+            string js = File.ReadAllText(path);
+            Book book = JsonConvert.DeserializeObject<Book>(js);
+            Title = book.Title;
+            Author = book.Author;
+            NumberOfPages = book.NumberOfPages;
+            PublishingYear = book.PublishingYear;
+        }
+    }
+}
---
+++ ConsumingData/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsumingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter and the Half-Blood Prince", "Joanne Rowling", 600, 2009);
+            Book book2 = new Book("The Little Prince", "Antoine de Saint-Exupéry", 104, 2003);
+            List<Book> books = new List<Book>();
+            books.Add(book1);
+            books.Add(book2);
+            int counter = 1;
+            foreach (var book in books)
+            {
+                book.SaveToXml(@"D:\ConsumingData\book" + (counter++).ToString() + ".xml");
+                book.SaveToJSON(@"D:\ConsumingData\book" + (counter++).ToString() + ".json");
+            }
+            foreach (var file in Directory.EnumerateFiles(@"D:\ConsumingData\").Where(x => x.EndsWith(".json")))
+            {
+                var book = new Book();
+                book.LoadFromJSON(file);
+                Console.WriteLine(book.Author);
+            }
+            foreach (var file in Directory.EnumerateFiles(@"D:\ConsumingData\").Where(x => x.EndsWith(".xml")))
+            {
+                var book = new Book();
+                book.ReadFromXml(file);
+                Console.WriteLine(book.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
